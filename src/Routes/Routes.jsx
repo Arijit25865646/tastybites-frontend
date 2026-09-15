813668eb0d0e8f25063be2cc99f8653ba36f655f
Arijit25865646 <arijitchowdhury734@gmail.com>
2026-09-16 00:28:43 +0530

    Fix Home page import case

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,7 +6,7 @@ import AdminProtected from "../components/AdminProtected";
 
 // ================= USER PAGES =================
 
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Menu from "../pages/Menu";
 import MenuDetails from "../pages/MenuDetails";
 import Login from "../pages/Login";
